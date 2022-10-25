4e69d2c8235a84b01a9ce47c92167bd037ef9385
Arnaud de Marans <arnaud.demarans@epsi.fr>
2022-10-25 09:17:19 +0200

    copie du fichier csv et code python ouvrir un fichier

---
+++ test.py
@@ -0,0 +1,13 @@
+#Ouverture d'un fichier CSV
+#Création des listes des lignes nommée tableau...
+#et affichage des lignes
+import csv
+with open ('conso-annuelles_v1.csv',newline='') as f: #ouverture du fichier csv
+    tableau=[]
+    lire=csv.reader(f)                                #Chargement des lignes du fichier csv
+    print('',end='\n')
+    print('Affichage des lignes du tableau',end='\n')
+    for ligne in lire:                                # Pour chaque ligne
+        print(ligne, end='\n')                        # afficher la ligne dans la console
+        tableau.append(ligne)                         # ajouter la ligne dans la liste
+                                                      # liste   nommée tableau
